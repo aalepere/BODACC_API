1f5fb908f7f01667dc6c6011f6861f04512a9cb8
Arnaud <arnaud@hokodo.co>
2020-04-05 22:23:13 +0200

    test fs

---
+++ utils/test/test_utils.py
@@ -0,0 +1,14 @@
+""" Test all functions in utils package """
+import unittest
+
+
+class TestUtils(unittest.TestCase):
+    """
+    OK
+    """
+
+    def testA(self):
+        """
+        OK
+        """
+        self.assertEqual(1, 1)
